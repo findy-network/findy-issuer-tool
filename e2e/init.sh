c41d6b1898036c285d8a528c7845c2da1d28535b
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-01-31 10:36:56 +0200

    Tune nightwatch config.

--- e2e/init.sh
+++ e2e/init.sh
@@ -8,5 +8,11 @@ cd $CURRENT_DIR
 mkdir -p .build
 git clone https://github.com/findy-network/findy-wallet-pwa.git .build/findy-wallet-pwa || echo "Already cloned"
 cd .build/findy-wallet-pwa/tools/env-docker && make clean && make init-store && docker-compose up -d
-cd ../../../../../api/ && npm run db && (npm start &)
-cd ../frontend && (npm run start:e2e &)
+
+auth_url="http://localhost:8088"
+auth_origin="http://localhost:3000"
+grpc_server="localhost:50052"
+tls_path="./tools/env/config/cert"
+
+timestamp=$(date +%s)
+user=user-$timestamp
--- e2e/test.js
+++ e2e/test.js
@@ -1,8 +1,20 @@
-const home = "http://localhost:8081";
+describe("Issuer Tool", function () {
+  const home = "http://localhost:8081";
 
-module.exports = {
-  "Check app loads": (browser) => {
-    const loginBtn = '//button[contains(.,"Dev login")]';
-    browser.url(home).waitForElementVisible(loginBtn).end();
-  },
-};
+  it("app loads", function (browser) {
+    browser
+      .url(home)
+      .waitForElementVisible("#dev-login-button")
+      .click("#dev-login-button")
+      .assert.textContains("h6", "Issuer Tool")
+      .end();
+  });
+
+  it("pairwise is done", function (browser) {
+    browser
+      .url(home)
+      .waitForElementVisible("#dev-login-button")
+      .click("#dev-login-button")
+      .end();
+  });
+});
