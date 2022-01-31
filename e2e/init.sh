aeed140c40ef679d71f0f3cb01308c0a82ef70fa
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-01-31 10:45:44 +0200

    Collect docker logs on failures.

--- e2e/init.sh
+++ e2e/init.sh
@@ -12,7 +12,7 @@ cd .build/findy-wallet-pwa/tools/env-docker && make clean && make init-store && 
 auth_url="http://localhost:8088"
 auth_origin="http://localhost:3000"
 grpc_server="localhost:50052"
-tls_path="./tools/env/config/cert"
+tls_path="./.build/findy-wallet-pwa/tools/env/config/cert"
 
 timestamp=$(date +%s)
 user=user-$timestamp
