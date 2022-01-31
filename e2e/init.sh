3867588e551fb7457e823a18e2957505ae741d3e
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-01-31 10:55:43 +0200

    Set cert path env variable.

--- e2e/init.sh
+++ e2e/init.sh
@@ -16,3 +16,24 @@ tls_path="./.build/findy-wallet-pwa/tools/env/config/cert"
 
 timestamp=$(date +%s)
 user=user-$timestamp
+default_key=$(findy-agent-cli new-key)
+
+echo "Running e2e test for $auth_url (origin: $auth_origin, api: $grpc_server)"
+
+# register web wallet user
+echo "Register user $user"
+findy-agent-cli authn register \
+    -u $user \
+    --url $auth_url \
+    --origin $auth_origin \
+    --key $default_key
+
+# login web wallet user
+echo "Login user $user"
+jwt=$(findy-agent-cli authn login \
+    -u $user \
+    --url $auth_url \
+    --origin $auth_origin \
+    --key $default_key)
+
+echo {\"jwt\": \"$jwt\", \"user\": \"$user\"} > ./e2e/e2e.user.json
