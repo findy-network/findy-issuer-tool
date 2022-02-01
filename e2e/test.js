faf2ed7c0121ebf311dedfd69e955b9de6a63fac
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-02-01 18:24:03 +0200

    Add test for proof request.

--- e2e/test.js
+++ e2e/test.js
@@ -4,6 +4,7 @@ describe("Issuer Tool", function () {
   const user = require("./e2e.user.json");
   const userCmd = `window.localStorage.token = "${user.jwt}"`;
   const issuerToolLabel = '//p[contains(.,"issuer-tool")]';
+  const acceptBtn = '//button[contains(.,"Accept")]';
 
   it("app loads", function (browser) {
     browser
@@ -81,7 +82,6 @@ describe("Issuer Tool", function () {
   });
 
   it("cred is sent", function (browser) {
-    const acceptBtn = '//button[contains(.,"Accept")]';
     const walletLink = '//a[contains(.,"Wallet")]';
     const credIcon = "svg[aria-label=Certificate]";
     browser
@@ -119,6 +119,43 @@ describe("Issuer Tool", function () {
       .click(walletLink)
       .useCss()
       .waitForElementVisible(credIcon)
+      .end();
+  });
+
+  it("cred is verified", function (browser) {
+    const proofSuccess = `//p[contains(.,"Proved attributes ${new Date().toLocaleDateString(
+      "et-EE"
+    )}")]`;
+    browser
+      // send proof request
+      .url(home)
+      .waitForElementVisible("#dev-login-button")
+      .click("#dev-login-button")
+      .waitForElementVisible("#verify-link-item")
+      .click("#verify-link-item")
+      .waitForElementVisible("#connection-selection")
+      .click("#connection-selection")
+      .waitForElementVisible("#connection-selection>option:nth-of-type(2)")
+      .click("#connection-selection>option:nth-of-type(2)") // just select the first one
+      .waitForElementVisible("#cred-def-selection")
+      .click("#cred-def-selection")
+      .waitForElementVisible("#cred-def-selection>option:nth-of-type(2)")
+      .click("#cred-def-selection>option:nth-of-type(2)") // just select the first one
+      // Use default values for this cred
+      .click("#send-cred-save-button")
+      .assert.textContains("div", "Proof request sent successfully")
+
+      // Accept proof
+      .url(walletHome)
+      .execute(userCmd)
+      .url(walletHome)
+      .useXpath()
+      .waitForElementVisible(issuerToolLabel)
+      .click(issuerToolLabel)
+      .waitForElementVisible(acceptBtn)
+      .click(acceptBtn)
+      .useXpath()
+      .waitForElementVisible(proofSuccess)
       .end();
   });
 });
