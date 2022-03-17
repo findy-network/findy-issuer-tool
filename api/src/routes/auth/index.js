267feb53a741bcf5ecf39e67769664cec3bcb3ed
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-03-17 10:04:16 +0200

    Fix credential sent note to credential fetched note.

--- api/src/routes/auth/index.js
+++ api/src/routes/auth/index.js
@@ -4,6 +4,7 @@ import github from './github';
 import dev from './dev';
 import isb from './isb';
 import findy from './findy';
+import log from '../../log';
 
 export default async (storage, config) => {
   const { addOrUpdateUser } = storage;
@@ -18,14 +19,19 @@ export default async (storage, config) => {
   const ghLogin = github(createToken, config);
   const devLogin = dev(createToken, config);
   const isbCred = await isb(addOrUpdateUser, config);
-  const findyLogin = await findy(createToken, config);
+  let findyLogin = null;
+  try {
+    findyLogin = await findy(createToken, config);
+  } catch (err) {
+    log.error('Unable to init findy login: ', err);
+  }
 
   const getConfig = (req) => {
     const conf = {
       auth: {
         dev: { url: devLogin.getUrl() },
         github: { url: ghLogin.getUrl() },
-        findy: { url: findyLogin.getUrl(req) },
+        ...(findyLogin ? { findy: { url: findyLogin.getUrl(req) } } : {}),
       },
       creds: {
         isb: { url: isbCred.getUrl() },
@@ -37,7 +43,7 @@ export default async (storage, config) => {
     getIntegrationConfig: getConfig,
     githubLogin: ghLogin.githubLoginIssuer,
     devModeLogin: devLogin.devLogin,
-    findyLogin: findyLogin.findyOIDCCallback,
+    findyLogin: findyLogin ? findyLogin.findyOIDCCallback : () => {},
     isbGetUrlForEmail: isbCred.getUrlForEmail,
     isbSendCred: isbCred.isbCallback,
   };
