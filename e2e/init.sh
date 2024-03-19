ed21009377354406a4dcb3f249594b8fdbe317f0
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2024-03-19 13:32:15 +0200

    Use ready build pwa image.

--- e2e/init.sh
+++ e2e/init.sh
@@ -8,7 +8,7 @@ CURRENT_DIR=$PWD
 
 mkdir -p .build
 git clone https://github.com/findy-network/findy-wallet-pwa.git .build/findy-wallet-pwa || echo "Already cloned"
-cd .build/findy-wallet-pwa/tools/env-docker && make clean && make init-store && docker-compose up -d
+cd .build/findy-wallet-pwa/tools/env && make clean && make init-store && docker-compose up -d
 
 auth_url="http://localhost:8088"
 auth_origin="http://localhost:3000"
